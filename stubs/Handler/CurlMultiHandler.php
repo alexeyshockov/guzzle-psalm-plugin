cc7694b67126bcc52661a9968300655c5ae4fd33
Jaik Dean <jaik@airtimerewards.com>
2019-12-16 11:04:52 +0000

    Add return types for handlers

--- stubs/Handler/CurlMultiHandler.php
+++ stubs/Handler/CurlMultiHandler.php
@@ -2,10 +2,19 @@
 
 namespace GuzzleHttp\Handler;
 
+use GuzzleHttp\Promise\PromiseInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
 class CurlMultiHandler
 {
     /**
      * @param array{handle_factory?:CurlFactoryInterface,select_timeout?:int|float} $options
      */
     public function __construct(array $options = []) {}
+
+    /**
+     * @psalm-return PromiseInterface<ResponseInterface>
+     */
+    public function __invoke(RequestInterface $request, array $options) {}
 }
