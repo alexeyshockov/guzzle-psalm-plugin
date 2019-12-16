bf164fdf3bac5338be081fad1282e6ed373bcee9
Jaik Dean <jaik@jaikdean.com>
2019-12-16 12:54:51 +0000

    Add return types for handlers (#3)

--- stubs/Handler/MockHandler.php
+++ stubs/Handler/MockHandler.php
@@ -2,10 +2,19 @@
 
 namespace GuzzleHttp\Handler;
 
+use GuzzleHttp\Promise\PromiseInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
 class MockHandler
 {
     /**
      * @psalm-variadic
      */
     public function append() {}
+
+    /**
+     * @psalm-return PromiseInterface<ResponseInterface>
+     */
+    public function __invoke(RequestInterface $request, array $options) {}
 }
